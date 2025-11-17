5318caab25f575bf27a859768a915d293a2ce514
Camille Ferrell <ferrell.c@northeastern.edu>
2025-11-17 13:06:17 -0500

    got the bluetooth working

--- MCU/main/main.c
+++ MCU/main/main.c
@@ -1,7 +1,7 @@
 #include "nimble.h"
 #include "imu_i2c.h"
 #include "common.h"
-
+#include "IRFlasher.h"
 
 
 void app_main(void) {
@@ -10,5 +10,14 @@ void app_main(void) {
 
     start_ble_task();
 
+    // start IR flasher task
+    xTaskCreate(
+        irflasher_task,      // Task function
+        "IR_Flasher",        // Name (for debugging)
+        4096,                // Stack size (bytes or words depending on config)
+        NULL,                // Task argument
+        5,                   // Priority
+        NULL                 // Task handle (optional)
+    );
 
 }
