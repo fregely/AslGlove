ca2ce0df3e0e3eded6e4afb627a973e6b4c1c93a
Clara Small <clarasmall63@gmail.com>
2025-12-01 19:38:58 -0500

    added more to letter detection

--- App/letter_recognizer.py
+++ App/letter_recognizer.py
@@ -41,12 +41,7 @@ class LetterRecognizer:
             self.is_near_thumb(thumb, middle)):
             if self.fingers_curled(y_vals):
                 return "A"
-
-        # Letter "L": thumb and index extended, others bent
-        if self.is_far(thumb, index) and self.is_bent(middle):
-            if self.is_bent(ring) and self.is_bent(pinky):
-                return "L"
-
+            
         # Letter "B": all fingers extended upward, thumb across palm
         if self.all_extended(y_vals) and self.is_near_thumb(thumb, index):
             return "B"
@@ -54,6 +49,81 @@ class LetterRecognizer:
         # Letter "C": curved spatial arc between thumb and pinky
         if self.c_curve_shape(thumb, index, middle, ring, pinky):
             return "C"
+        
+        # Letter "D": pointer extended, others making o shape with thumb
+        if (self.finger_extended(index, y_vals) and 
+            self.is_near_thumb(thumb, middle) and self.is_near_thumb(thumb, ring) 
+            and self.is_near_thumb(thumb, pinky)):
+            if self.fingers_curled(y_vals):
+                return "D"
+            
+        # Letter "E": all fingers bent, varies based on person
+
+        # Letter "F": middle, ring, and pinky extended, pointer and thumb curled together
+
+        # Letter "G": pointer finger extended to the side
+            #need to check if it's to the side versus original orientation of hand?
+            #middle, ring, pinky curled into hand
+
+        # Letter "H": pointer and middle extended to the side
+            #ring and pinky curled into hand
+
+        # Letter "I" and "J": only pinky extended, pointer, middle, ring curled into hand
+            #no idea how to do J, need to watch the SEQUENTIAL datas if pinky swings down
+
+        # Letter "K": pointer and middle extended and far apart
+            #the difference between this and V is that the thumb is also extended 
+            # (in between pointer and middle)
+
+        # Letter "L": thumb and index extended, others bent
+        if self.is_far(thumb, index) and self.is_bent(middle):
+            if self.is_bent(ring) and self.is_bent(pinky):
+                return "L"
+            
+        # Letter "M": 
+
+        # Letter "N":
+
+        # Letter "O": all fingers curled, close to thumb
+        if (self.fingers_curled(y_vals) and self.is_near_thumb(thumb, index) and 
+            self.is_near_thumb(thumb, middle) and self.is_near_thumb(thumb, ring)
+            and self.is_near_thumb(thumb, pinky)):
+            return "O"
+
+        # Letter "P":
+
+        # Letter "Q":
+
+        # Letter "R": pointer and middle extended up together, curled around each other
+            #other fingers bent
+            #NEED TO DECIPER FROM U
+        if self.is_bent(ring) and self.is_bent(pinky) and self.is_close(index, middle):
+            return "R"
+
+        # Letter "S":
+
+        # Letter "T":
+
+        # Letter "U": literally the same as R but pointer and middle not curled around each other
+        if self.is_bent(ring) and self.is_bent(pinky) and self.is_close(index, middle):
+            return "U"
+
+        # Letter "V": ring and pinky curled, pointer and middle extended but far apart
+
+        # Letter "W": pinky curled, pointer, middle, and ring extended
+
+        # Letter "X": pointer up but bent (like a hook), all others curled
+            #need to deciper from D and X
+            #maybe have the initial stretched out hand position, that one is "D", and if
+            #pointer finger is lower than that but still extended it's "X"
+
+        # Letter "Y": pointer, middle, ring curled, thumb and pinky extended
+        if self.finger_extended(thumb, y_vals) and self.finger_extended(pinky, y_vals):
+            if self.is_bent(index) and self.is_bent(middle) and self.is_bent(ring):
+                return "Y"
+
+        # Letter "Z": check with D
+
 
         return None
 
@@ -69,6 +139,9 @@ class LetterRecognizer:
 
     def is_far(self, p1, p2, thresh=0.05):
         return self.dist(p1, p2) > thresh
+    
+    def is_close(self, p1, p2, thresh=0.025):
+        return self.dist(p1,p2) < thresh
 
     def fingers_curled(self, y_vals):
         """
@@ -92,3 +165,16 @@ class LetterRecognizer:
             self.dist(thumb, pinky) > 0.04 and
             abs(self.dist(idx, mid) - self.dist(ring, pinky)) < 0.02
         )
+    
+    def finger_extended(self, finger, all_fingers, diff=0.015):
+        """
+        finger : (x,y,z) tuple for the finger you're checking
+        all_fingers : list of (x,y,z) for index, middle, ring, pinky
+        diff : minimum y separation to count as extended
+        """
+        finger_y = finger[1]
+
+        ys = [f[1] for f in all_fingers]
+        avg_other = sum(ys) / len(ys)
+
+        return (avg_other - finger_y) > diff
